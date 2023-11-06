ffcadeda6d5671f763718351cf4387feab9fc16b
Emma Nasseri <enasseri02@gmail.com>
2023-11-06 15:26:43 -0600

    initial set up

---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+"use client";
+
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import StyledButton from "./StyledButton";
+
+//import styles from "./Navbar.module.css"; // Import the CSS module
+
+const Navbar = () => {
+  return (
+    <nav>
+      <Link href="/">
+        <StyledButton title="home" />
+      </Link>
+
+      <div>
+        <Link href="/play">
+          <StyledButton title="play jeopardy" />
+        </Link>
+        <Link href="/about">
+          <StyledButton title="about this project" />
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/components/StyledButton.js
@@ -0,0 +1,56 @@
+import React from "react";
+import Link from "next/link";
+
+const StyledButton = ({ title, onClick, link }) => {
+  if (link) {
+    return (
+      <Link href={link} style={styles.buttonLink}>
+        {title}
+      </Link>
+    );
+  }
+
+  if (onClick && typeof onClick === "function") {
+    return (
+      <button style={styles.button} onClick={onClick}>
+        {title}
+      </button>
+    );
+  }
+
+  return <button style={styles.button}>{title}</button>;
+};
+
+export default StyledButton;
+const styles = {
+  button: {
+    border: "2px solid white",
+    borderRadius: "8px",
+    padding: "8px 16px",
+    backgroundColor: "transparent",
+    color: "white",
+    cursor: "pointer",
+    outline: "none",
+    transition: "0.3s",
+    "&:hover": {
+      backgroundColor: "black",
+      color: "white",
+    },
+  },
+  buttonLink: {
+    display: "inline-block",
+    border: "2px solid black",
+    borderRadius: "8px",
+    padding: "8px 16px",
+    backgroundColor: "transparent",
+    color: "white",
+    cursor: "pointer",
+    textDecoration: "none",
+    outline: "none",
+    transition: "0.3s",
+    "&:hover": {
+      backgroundColor: "black",
+      color: "white",
+    },
+  },
+};
